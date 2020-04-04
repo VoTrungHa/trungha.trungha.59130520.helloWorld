2aa3915345f78a9961735d040d363f51cc1dbc47
VoTrungHa <trinhsatvth@gmail.com>
2020-04-04 03:22:11 -0700

    Myapp_VoTrungHa

---
+++ src/main/java/vn/edu/ntu/trungha/Trungha_59130520_helloWorld/MainActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.ntu.trungha.Trungha_59130520_helloWorld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
